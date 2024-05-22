9cf908e6e40c009fb65cdcc9d1f4abf83d7447a6
dinhdung <hoangdinhdung@hoasao.com.vn>
2024-05-22 09:26:36 +0700

    add cors

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // USE MIDDLEWARES
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(
